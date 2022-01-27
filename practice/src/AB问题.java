3a5156d238a61ed88edfde0668277fb3fc49fe42
kevin wang <wangsir0716@qq.com>
2022-01-27 19:03:45 +0800

    'abwenti'

---
+++ practice/src/AB问题.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AB问题 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer a = scanner.nextInt();
+        Integer b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
